3bdaf8a3d1a040876420b61aff79651963039bba
haphuocsang1801@gmail.com <haphuocsang1801@gmail.com>
2022-08-11 18:53:23 +0700

    complete home page

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+const login = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae odit
+      consequuntur sed pariatur aperiam assumenda, illo accusantium adipisci,
+      voluptates eum ullam nihil eos aspernatur? Consequatur dicta quis
+      accusantium libero est.
+    </div>
+  );
+};
+
+export default login;
